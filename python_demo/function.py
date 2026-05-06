2304de725a8b79a2908826df0a26ae69c21e8e16
upendher5G1 <uppyuppy020@gmail.com>
2026-05-06 22:35:29 +0530

    python calculator USING functions

---
+++ python_demo/function.py
@@ -0,0 +1,20 @@
+def calculator():
+    print("....Simple Calculator....")
+    print("Operations: +  -  *  /")
+    num1 = float(input("Enter first number: "))
+    op = input("Enter operator: ")
+    num2 = float(input("Enter second number: "))
+    if op == "+":
+        print("Result:", num1 + num2)
+    elif op == "-":
+        print("Result:", num1 - num2)
+    elif op == "*":
+        print("Result:", num1 * num2)
+    elif op == "/":
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Error: Division by zero")
+    else:
+        print("Invalid operator")
+calculator()
