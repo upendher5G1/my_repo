eab0d9fa709306368d8785f7c64e81ef07b5121e
upendher5G1 <uppyuppy020@gmail.com>
2026-05-07 16:01:36 +0530

    javascript Updated calculator

--- demo_js/calculator.js
+++ demo_js/calculator.js
@@ -1,19 +1,26 @@
 const prompt=require("prompt-sync")();
-let num1 = parseFloat(prompt("Enter first number:"));
-let num2 = parseFloat(prompt("Enter second number:"));
-let op = prompt("Enter operator (+, -, *, /):");
-if (op === "+") {
-    console.log("Result:"+(num1+num2));
-} else if (op === "-") {
-    console.log("Result:"+(num1-num2));
-} else if (op === "*") {
-    console.log("Result:"+(num1*num2));
-} else if (op === "/") {
-    if (num2 !== 0) {
-        console.log("Result:"+(num1/num2));
+while(true){
+    console.log("....Simple Calculator....");
+    console.log("Enter 'x' For Exit Calculator")
+    let num1 = parseFloat(prompt("Enter first number:"));
+    let num2 = parseFloat(prompt("Enter second number:"));
+    let op = prompt("Enter operator (+, -, *, /):");
+    if (op === "+") {
+        console.log("Result:"+(num1+num2));
+    } else if (op === "-") {
+        console.log("Result:"+(num1-num2));
+    } else if (op === "*") {
+        console.log("Result:"+(num1*num2));
+    } else if (op === "/") {
+        if (num2 !== 0) {
+            console.log("Result:"+(num1/num2));
+        } else {
+            console.log("Error: Cannot Divide by Zero");
+        }
+    }else if (op === "x") {
+            console.log("calculator closed...");
+            break;
     } else {
-        console.log("Error: Cannot Divide by Zero");
+        console.log("Invalid operator");
     }
-} else {
-    console.log("Invalid operator");
 }
